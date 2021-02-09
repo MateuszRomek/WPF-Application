5b45e97092ed974199782cc740797dd9b87a48ab
Mateusz <mateusz.romek@outlook.com>
2021-02-09 20:19:42 +0100

    First setup of base view models and views.

---
+++ WPFProjekt/ViewModels/EditViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace WPFProjekt.ViewModels
+{
+    public class EditViewModel : BaseViewModel
+    {
+    }
+}
---
+++ WPFProjekt/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WPFProjekt.Commands;
+namespace WPFProjekt.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public BaseViewModel SelectedViewModel { get; set; }
+
+        public ICommand UpdateViewCommand { get; set; }
+
+        public MainViewModel()
+        {
+            UpdateViewCommand = new UpdateViewCommand(this);
+        }
+    }
+}
