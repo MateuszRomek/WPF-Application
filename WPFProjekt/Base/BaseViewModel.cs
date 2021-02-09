ab9f0111a2efc3cfd8515354ed123dc0b055307c
Mateusz <mateusz.romek@outlook.com>
2021-02-09 20:18:15 +0100

    Created base folder with base model view

---
+++ WPFProjekt/Base/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFProjekt.ViewModels
+{
+  
+    public  class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+    }
+}
