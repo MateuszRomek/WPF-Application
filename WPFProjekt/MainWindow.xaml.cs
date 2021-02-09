440baf3d96ea87b5eae21b209c88a739f0a5c8d9
Mateusz <mateusz.romek@outlook.com>
2021-02-09 20:21:03 +0100

    Installed packed PropertyChanged Fody to reduce boilerplate for property changed event implemented in BaseModelView

--- WPFProjekt/MainWindow.xaml.cs
+++ WPFProjekt/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPFProjekt.ViewModels;
 
 namespace WPFProjekt
 {
@@ -23,6 +24,7 @@ namespace WPFProjekt
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
