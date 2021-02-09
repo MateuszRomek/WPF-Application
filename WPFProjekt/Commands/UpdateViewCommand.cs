a881da467f7e35574e061d1f0ea4224f2e6df9bc
Mateusz <mateusz.romek@outlook.com>
2021-02-09 20:18:52 +0100

    Created update command view

---
+++ WPFProjekt/Commands/UpdateViewCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WPFProjekt.ViewModels;
+
+namespace WPFProjekt.Commands
+{
+    public class UpdateViewCommand : ICommand
+    {
+
+        private MainViewModel viewModel;
+
+        public event EventHandler CanExecuteChanged;
+    
+        public UpdateViewCommand(MainViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter.ToString() == "List")
+            {
+                viewModel.SelectedViewModel = new ListViewModel();
+            } else if(parameter.ToString() == "EditList")
+            {
+                viewModel.SelectedViewModel = new EditViewModel();
+            }
+           
+        }
+    }
+}
