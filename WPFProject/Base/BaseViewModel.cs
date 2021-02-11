57b228a3f817402d77062bc3ee7822c8f8f9bef1
Mateusz <mateusz.romek@outlook.com>
2021-02-11 18:20:57 +0100

    Setup folder structure

---
+++ WPFProject/Base/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace WPFProject.Base
+{
+    /// <summary>
+    ///    BaseViewModel from which other ViewModels will inherit
+    /// </summary>
+    /// 
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        ///    Method that will update view model
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+    }
+}
